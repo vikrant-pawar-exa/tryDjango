da28957589f262d04ff693d5efee3c817b445313
Amol Dumbre <amol.dumbre@gslab.com>
2020-02-13 15:02:29 +0530

    Set up .flaskenv for development environments

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 class Config(object):
-    FLASK_APP='run.py'
     DEBUG = False
     TESTING = False
-    DATABASE_URI = 'mongo://user@localhost/foo'
 
 class ProductionConfig(Config):
     DATABASE_URI = 'mongo://user@localhost/foo'
