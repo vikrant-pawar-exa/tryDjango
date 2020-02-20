ecf7c80fe3664ce6c6cd0974972b7a11d9aa5be9
Vikrant Pawar <vikrant.pawar@gslab.com>
2020-02-20 21:11:50 +0530

    Fixing log files
    Signed-off-by: Vikrant Pawar <vikrant.pawar@gslab.com>

--- config.py
+++ config.py
@@ -3,8 +3,10 @@ class Config(object):
     TESTING = False
     USERNAME_JIRA = ""
     PASSWORD_JIRA = ""
-    HOST_JIRA = ""
+    HOST_JIRA = "exabeam.atlassian.net"
     OKTA_HOST_URL = 'https://dev-780755.okta.com'
+    EXA_SECURITY = ""
+
 
 class ProductionConfig(Config):
     OKTA_HOST_URL = 'https://dev-780755.okta.com'
