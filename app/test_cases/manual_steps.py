c508c279c5ff297a895cff678a472fa8924c5122
Priyanka Deokate <priyanka.deokate@gslab.com>
2020-02-20 17:10:56 +0530

    Fixed import file issue for lime

--- app/test_cases/manual_steps.py
+++ app/test_cases/manual_steps.py
@@ -2,7 +2,7 @@ from flask import jsonify, request
 from flask_restful import Resource
 from app.utils.lime_file_converter import LimeConverter
 
-class LimeSetup(Resource):
+class Lime(Resource):
 
     def post(self):
         json_data = request.get_json(force=True)
