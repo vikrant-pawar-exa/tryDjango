5c999906e0ff7903cb94ba1cdb8d9c6209391b98
Pragati Wagh <pragati.wagh@gslab.com>
2020-02-25 11:42:53 +0530

    Addressing to review comments. Changes in the return type and add the details regarding to the input to the methods.

--- app/utils/tools_tickets_audit.py
+++ app/utils/tools_tickets_audit.py
@@ -8,13 +8,14 @@ class ToolsTicketAudit:
 
 	def create_audit(tool_details):
 		try:
+			import pdb; pdb.set_trace()
 			tool_details['tool_executed'] = Constants.TOOLS.index(tool_details['tool_executed'].upper())
-			tool_details['completion_time'] = str(datetime.now)
+			tool_details['completion_time'] = str(datetime.now())
 			client.tools_and_ticket_logging.insert_one(tool_details)
 			return 'success'
 		except Exception as e:
 			logging.error("----Error while decryption : {}".format(type(e)))
-			return 'failure'
+			raise e
 
 	def get_audit_by_useremail(email):
 		try:
@@ -22,12 +23,19 @@ class ToolsTicketAudit:
 			return audit_logs
 		except Exception as e:
 			logging.error("----Error while decryption : {}".format(type(e)))
-			return None
+			raise e
 
-	def get_audit_by_useremail(ticket_id):
+	def get_audit_by_ticket(ticket_id):
 		try:
 			audit_logs = client.tools_and_ticket_logging.find({'ticket_id': ticket_id},  {'_id': False})
 			return audit_logs
 		except Exception as e:
 			logging.error("----Error while decryption : {}".format(type(e)))
-			return None
+			raise e
+# 
+
+# 	def test():
+# 		tool_details = { 'ticket_id' : 'CONT-2383', 'user_email' : 'pragati@gslab.com', 'tool_executed': 'lime-aa', 'output'  : 'success'}
+#			tools executed can have values like : lime-aa, lime-dl, sbt, sake (case insensitive)
+# 		ToolsTicketAudit.create_audit(tool_details)
+# #
