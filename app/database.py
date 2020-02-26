2f39ff593038a3f4ada0128175091c512f32c206
Pragati Wagh <pragati.wagh@gslab.com>
2020-02-26 15:03:16 +0530

    Add authentication for mongo db database.

--- app/database.py
+++ app/database.py
@@ -3,8 +3,6 @@ from config import Config
 import os
 
 #client_connection = MongoClient('mongodb://localhost:27017')
-client_connection = MongoClient(Config.DB_HOST)
+client_connection = MongoClient(Config.DB_HOST, username='ca-data-admin', password='exabeam-ca-admin', authSource='ca-data', authMechanism='SCRAM-SHA-256')
 
-client = client_connection['testdatabase']
-
-
+client = client_connection['ca-data']
