4ef7703637441e7a5b802fc0d107422b0a682f78
Vikrant Pawar <vikrant.pawar@gslab.com>
2020-02-26 12:54:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
     HOST_JIRA = "exabeam.atlassian.net"
     DB_HOST = "mongodb://db:27017"
     OKTA_HOST_URL = 'https://dev-780755.okta.com'
-    EXA_SECURITY = ""
+    EXA_SECURITY = "/home/exa_security"
 
 
     EXABEAM_HOME ="/test/exa_security/martini/"
