d4103b3e5d34b4cb73b4d0b2abf7d5fd8a40d83d
Pragati Wagh <pragati.wagh@gslab.com>
2020-02-26 15:53:19 +0530

    Changes in database config.

--- app/database.py
+++ app/database.py
@@ -1,8 +1,11 @@
 from pymongo import MongoClient
 from config import Config
+from dotenv import load_dotenv
+load_dotenv()
 import os
 
 #client_connection = MongoClient('mongodb://localhost:27017')
-client_connection = MongoClient(Config.DB_HOST, username='ca-data-admin', password='exabeam-ca-admin', authSource='ca-data', authMechanism='SCRAM-SHA-256')
+client_connection = MongoClient(Config.DB_HOST, username='ca-data-admin', password=os.getenv('CA_DATA_ADMIN_PASSWORD'), authSource='ca-data', authMechanism='SCRAM-SHA-256')
 
 client = client_connection['ca-data']
+
